70dd066dd054d24709b038cb187ab935bc62afce
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-03-27 18:20:06 +0530

    add dummy include helpers

---
+++ boys/allboys.h
@@ -0,0 +1,10 @@
+// Dummy header file to include all boys
+
+#ifndef ALLBOYS_H
+#define ALLBOYS_H
+
+#include "geekboy.h"
+#include "generousboy.h"
+#include "miserboy.h"
+
+#endif
---
+++ girls/allgirls.h
@@ -0,0 +1,10 @@
+// Dummy header file to include all girl types
+
+#ifndef ALLGIRLS_H
+#define ALLGIRLS_H
+
+#include "choosygirl.h"
+#include "normalgirl.h"
+#include "desperategirl.h"
+
+#endif /* ALLGIRLS_H */
