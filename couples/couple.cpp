6283c42258f75d7a356f78b2fee8bb37c63afe00
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-03-28 13:18:07 +0530

    removed this-> wherever possible to reduce code length

--- couples/couple.cpp
+++ couples/couple.cpp
@@ -9,7 +9,8 @@ bool compareOnHappiness (Couple c1, Couple c2)
     return (c1.findHappiness() > c2.findHappiness());
 }
 
-bool compareOnCompatibility(Couple c1, Couple c2) {
+bool compareOnCompatibility(Couple c1, Couple c2)
+{
     return (c1.findCompatibility() > c2.findCompatibility());
 }
 
@@ -18,18 +19,18 @@ bool giftRemovalPred (Gift g)
     return (g.gifted || g.type != luxury);
 }
 
-Couple::Couple(Boy b, Girl g)
+Couple::Couple(Boy *b, Girl *g)
 {
-    this->boy = b;
-    this->girl = g;
-    this->boy.committed = true;
-    this->girl.committed = true;
+    boy = b;
+    girl = g;
+    boy->committed = true;
+    girl->committed = true;
 }
 
 float Couple::findHappiness()
 {
-    float boyHapp = this->boy.getHappiness();
-    float girlHapp = this->girl.getHappiness();
+    float boyHapp = boy->getHappiness();
+    float girlHapp = girl->getHappiness();
     return boyHapp + girlHapp;
 }
 
@@ -37,60 +38,58 @@ void Couple::makeGiftBasket(std::vector<Gift> giftsList, Logger *logger)
 {
     if (giftsList.empty()) return;
 
-    this->gifts.clear();
+    gifts.clear();
 
     /* This is the maximum money to be spent.
        It is different for different types of boys.
      */
     int expenseLimit = 0;
 
-    switch (this->boy.nature) {
+    switch (boy->getNature()) {
     case miser:
     case geek:
-        expenseLimit = this->girl.budget;
+        expenseLimit = girl->budget;
         std::sort (giftsList.begin(), giftsList.end(), compareOnPriceAsc);
         break;
 
     case generous:
-        expenseLimit = this->boy.budget;
+        expenseLimit = boy->budget;
         std::sort (giftsList.begin(), giftsList.end(), compareOnPriceDesc);
         break;
     }
 
     /* start forming the basket */
-    for (std::vector<Gift>::iterator it = giftsList.begin();
-         it != giftsList.end() && expenseLimit > 0;
-         it++) {
+    for (auto it = giftsList.begin(); it != giftsList.end() && expenseLimit > 0;  it++) {
         expenseLimit -= (*it).price;
-        this->gifts.push_back(*it);
+        gifts.push_back(*it);
         (*it).gifted = true;
-        logger->log("Gift", this->boy.name+" gifted "+(*it).name + " to "+ this->girl.name, true);
+        logger->log("Gift", boy->name+" gifted "+(*it).name + " to "+ girl->name, true);
     }
 
     /* if no gifts have been given, then raise the budget of the boy */
     if (gifts.empty()) {
-        this->boy.budget = expenseLimit = gifts[0].price;
-        this->gifts.push_back(gifts[0]);
-        logger->log("Budget", this->boy.name+ " raised his budget to "+ std::to_string(this->boy.budget), true);
-        logger->log("Gift", this->boy.name+" gifted "+ gifts[0].name + " to "+ this->girl.name, true);
+        boy->budget = expenseLimit = gifts[0].price;
+        gifts.push_back(gifts[0]);
+        logger->log("Budget", boy->name+ " raised his budget to "+ std::to_string(boy->budget), true);
+        logger->log("Gift", boy->name+" gifted "+ gifts[0].name + " to "+ girl->name, true);
     }
 
     /* geek boys want to give a luxury gift */
-    if (expenseLimit > 0 && this->boy.nature == geek) {
+    if (expenseLimit > 0 && boy->getNature() == geek) {
         /* find a luxury gift */
         std::remove_if(giftsList.begin(), giftsList.end(), giftRemovalPred);
         if (! giftsList.empty())
-            this->gifts.push_back(giftsList[0]);
-        logger->log("Luxury", this->boy.name+" gifted luxury gift "+ gifts[0].name + " to "+ this->girl.name, true);
+            gifts.push_back(giftsList[0]);
+        logger->log("Luxury", boy->name+" gifted luxury gift "+ gifts[0].name + " to "+ girl->name, true);
     }
 
-    this->boy.setGiftBasket(&this->gifts);
-    this->girl.setGiftBasket(&this->gifts);
+    boy->gifts = &this->gifts;
+    girl->gifts = &this->gifts;
 }
 
 float Couple::findCompatibility()
 {
-    return (this->boy.budget - this->girl.budget) +
-        abs(this->boy.attr   - this->girl.attr)   +
-        abs(this->boy.intel  - this->girl.intel);
+    return (boy->budget - girl->budget) +
+        abs(boy->attr   - girl->attr)   +
+        abs(boy->intel  - girl->intel);
 }
--- couples/couple.h
+++ couples/couple.h
@@ -1,8 +1,8 @@
 #ifndef COUPLE_H
 #define COUPLE_H
 
-#include "../boys/boy.h"
-#include "../girls/girl.h"
+#include "../boys/allboys.h"
+#include "../girls/allgirls.h"
 #include "../gifts/gift.h"
 #include "../utils/logger/logger.h"
 
@@ -13,14 +13,14 @@ private:
 public:
     std::vector <Gift> gifts;   /*!< the vector of gifts for this couple */
     float happiness;            /*!< The happpiness of this couple */
-    Boy boy;                    /*!< This is the boy */
-    Girl girl;                  /*!< This is the girl */
-    Couple(Boy b, Girl g);      /*!< Constructor */
+    Boy *boy;                   /*!< This is the boy */
+    Girl *girl;                 /*!< This is the girl */
+    Couple(Boy *b, Girl *g);    /*!< Constructor */
     float findHappiness();      /*!< Find the happiness of this couple */
     float findCompatibility();  /*!< Find the compatibility of this couple */
 
     /** Form the gift basket (vector) for this couple from given gift list */
-    void makeGiftBasket(std::vector<Gift> gifts, Logger *logger);
+    void makeGiftBasket(std::vector<Gift> giftlist, Logger *logger);
 };
 
 
@@ -33,5 +33,6 @@ bool compareOnCompatibility(Couple c1, Couple c2);
  * For sorting in descending order
  */
 bool compareOnHappiness(Couple c1, Couple c2);
+
 
 #endif
