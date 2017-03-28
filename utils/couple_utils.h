a14d1e0031d2055a22fb814b82ceb069cda2bc50
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-03-28 13:52:49 +0530

    update couple_utils for new classes

--- utils/couple_utils.h
+++ utils/couple_utils.h
@@ -2,6 +2,8 @@
  */
 
 #include "../couples/couple.h"
+#include "../boys/allboys.h"
+#include "../girls/allgirls.h"
 #include <algorithm>
 
 #include "logger/logger.h"
@@ -9,39 +11,39 @@
 /** Form couples from given list of boys and girls \n
  * Returns the vector of couples formed
  */
-std::vector<Couple> makeCouples (std::vector<Boy> *boys,
-                                 std::vector<Girl> *girls, Logger *logger)
+std::vector<Couple> makeCouples (std::vector<Boy*> boys,
+                                 std::vector<Girl*> girls, Logger *logger)
 {
     std::vector<Couple> couples;
 
     int i, j;
-    for (i = 0; i < (int)(*girls).size(); i++) {
-        if ((*girls)[i].committed) continue;
+    for (i = 0; i < (int)girls.size(); i++) {
+        if (girls[i]->committed) continue;
 
         /* Sort the boys as required */
-        switch ((*girls)[i].boy_type) {
+        switch (girls[i]->boy_type) {
         case attractive:
-            std::sort((*boys).begin(), (*boys).end(), compareOnAttractiveness);
+            std::sort(boys.begin(), boys.end(), compareOnAttractiveness);
             break;
         case rich:
-            std::sort((*boys).begin(), (*boys).end(), compareOnRichness);
+            std::sort(boys.begin(), boys.end(), compareOnRichness);
             break;
         case intelligent:
-            std::sort((*boys).begin(), (*boys).end(), compareOnIntelligence);
+            std::sort(boys.begin(), boys.end(), compareOnIntelligence);
             break;
         }
 
         /* Find an appropriate boy */
-        for (j = 0; j < (int)(*boys).size(); j++) {
-            if ((*boys)[j].committed) continue;
-            logger->log("Match", "Checking compatibility of "+(*boys)[j].name+" and "+(*girls)[i].name, false);
-            if ((*boys)[j].isCompatible( (*girls)[i] ) &&
-                (*girls)[i].isCompatible( (*boys)[j] ) ) {
+        for (j = 0; j < (int)boys.size(); j++) {
+            if (boys[j]->committed) continue;
+            logger->log("Match", "Checking compatibility of "+boys[j]->name+" and "+girls[i]->name, false);
+            if (boys[j]->isCompatible( girls[i] ) &&
+                girls[i]->isCompatible( boys[j] ) ) {
 
-                couples.push_back( Couple((*boys)[j], (*girls)[i]) );
-                (*boys)[j].committed = true;
-                (*girls)[i].committed = true;
-                logger->log("Committed", (*boys)[j].name+" and "+(*girls)[i].name + " committed", false);
+                couples.push_back( Couple((boys)[j], girls[i]) );
+                boys[j]->committed = true;
+                girls[i]->committed = true;
+                logger->log("Committed", boys[j]->name+" and "+girls[i]->name + " committed", false);
                 break;
             }
         }
